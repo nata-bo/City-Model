86f4fee96dd62440d72ec7f0ba976df4670c308e
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-18 15:03:09 +0200

    add new code

--- src/Main.java
+++ src/Main.java
@@ -61,33 +61,66 @@ public class Main {
         Person john = new Person("John", "Black");
         Person jack = new Person("Jack", "Tamp");
 
-        Land land1 = new Land(ivan, "54:35:091455:15", 15, 20000,"Lemberg" ,Purpose.SETTLEMENT);
-        Land land2 = new Land(lena, "54:35:184532:15", 5, 500,"Belgorod",Purpose.AGRICULTURAL);
-        Land land3 = new Land(sonya, "54:35:425753:11", 20, 120000,"Bamberg",Purpose.INDUSTRIAL);
+        Land land1 = new Land(ivan, "54:35:091455:15", 15, 20000, "Lemberg", Purpose.SETTLEMENT);
+        Land land2 = new Land(lena, "54:35:184532:15", 5, 500, "Belgorod", Purpose.AGRICULTURAL);
+        Land land3 = new Land(sonya, "54:35:425753:11", 20, 120000, "Bamberg", Purpose.INDUSTRIAL);
         System.out.println(land1.getPropertyTax());
         System.out.println(land2.getPropertyTax());
         System.out.println(land3.getPropertyTax());
 
-        Forest forest1 = new Forest(john, "34:75:1534:97", 120, 250000,"Kiev", false);
-        Forest forest2 = new Forest(jack, "64:81:4738:10", 220, 1250000,"Munchberg", true);
+        Forest forest1 = new Forest(john, "34:75:1534:97", 120, 250000, "Kiev", false);
+        Forest forest2 = new Forest(jack, "64:81:4738:10", 220, 1250000, "Munchberg", true);
         System.out.println(forest1.getPropertyTax());
         System.out.println(forest2.getPropertyTax());
 
         ArrayList<Person> residentsList = new ArrayList<>(List.of(ivan, lena, sonya, jack, john));
         ArrayList<Person> residentsList2 = new ArrayList<>(List.of(ivan, sonya));
 
-        House house1 = new House(jack, "38:18:23612:32", 350, 980000, "Lviv",3, residentsList);
+        House house1 = new House(jack, "38:18:23612:32", 350, 980000, "Lviv", 3, residentsList);
         System.out.println(house1.getPropertyTax());
 
-        Apartment apartment1 = new Apartment(ivan, "21:47:92306:21", 100, 250000,"Seattle",3, residentsList2);
+        Apartment apartment1 = new Apartment(ivan, "21:47:92306:21", 100, 250000, "Seattle", 3, residentsList2);
         System.out.println(apartment1.getPropertyTax());
 
-        Garage garage1 = new Garage(lena, "10:32:64321:47", 25, 10000, "Bremen",1);
+        Garage garage1 = new Garage(lena, "10:32:64321:47", 25, 10000, "Bremen", 1);
         System.out.println(garage1.getPropertyTax());
 
-        RealProperty[] arrayOfEstates = {land1,land2,land3,forest1,forest2,house1,apartment1,garage1};
+        ArrayList<RealProperty> arrayOfEstates = new ArrayList<>(List.of
+                (land1, land2, land3, forest1, forest2, house1, apartment1, garage1)
+        );
+//        arrayOfEstates.sort(new PropertyByAddressComparator());
+//        System.out.println(arrayOfEstates);
+//        arrayOfEstates.sort(new PropertyByTaxAmountComparator());
+//        System.out.println(arrayOfEstates);
+//        arrayOfEstates.sort(new PropertyByAreaComparator());
+//        System.out.println(arrayOfEstates);
+//        arrayOfEstates.sort(new PropertyByTotalPriceComparator());
+//        System.out.println(arrayOfEstates);
 
-
-
+        getSortedByPriceList(arrayOfEstates);
+        System.out.println(arrayOfEstates);
+        getSortedByListTaxAmount(arrayOfEstates);
+        System.out.println(arrayOfEstates);
+        getSortedListByAddress(arrayOfEstates);
+        System.out.println(arrayOfEstates);
+        getSortedListByArea(arrayOfEstates);
+        System.out.println(arrayOfEstates);
     }
+
+    public static void getSortedByPriceList(ArrayList<RealProperty> list) {
+        list.sort(new PropertyByTotalPriceComparator());
+    }
+
+    public static void getSortedListByArea(ArrayList<RealProperty> list) {
+        list.sort(new PropertyByAreaComparator());
+    }
+
+    public static void getSortedByListTaxAmount(ArrayList<RealProperty> list) {
+        list.sort(new PropertyByTaxAmountComparator());
+    }
+
+    public static void getSortedListByAddress(ArrayList<RealProperty> list) {
+        list.sort(new PropertyByAddressComparator());
+    }
+
 }
