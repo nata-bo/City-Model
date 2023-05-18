752a3e55aa7cf6d1baf66d852755d41e7331f822
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-18 17:56:23 +0200

    add method addHouse

--- src/Land.java
+++ src/Land.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 public class Land extends RealProperty {
     private final Purpose purpose;
+    ArrayList<House> houses = new ArrayList<>();
 
     public Land(Person owner, String cadastralNumber, double area, double price, String address, Purpose purpose) {
         super(owner, cadastralNumber, area, price, address);
         this.purpose = purpose;
     }
+
 
     @Override
     public String toString() {
@@ -21,5 +25,15 @@ public class Land extends RealProperty {
             case INDUSTRIAL -> getPrice() * 0.005;
         };
     }
+
+    public Purpose getPurpose() {
+        return purpose;
+    }
+
+    public void addHouse(House house){
+        houses.add(house);
+    }
+
+
 
 }
