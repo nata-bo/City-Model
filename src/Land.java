95eecd7cfe8e6fb8655d32a571f0910b7fb9b351
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-18 12:05:58 +0200

    add class Land

---
+++ src/Land.java
@@ -0,0 +1,25 @@
+public class Land extends RealProperty{
+  private Purpose purpose;
+
+    public Land(Person owner, String cadastralNumber, double area, double price, Purpose purpose) {
+        super(owner, cadastralNumber, area, price);
+        this.purpose = purpose;
+    }
+
+    @Override
+    public String toString() {
+        return "Land{" +
+                "purpose=" + purpose +
+                "} " + super.toString();
+    }
+
+    public double getPropertyTax(){
+        return switch (purpose) {
+            case AGRICULTURAL -> getPrice() * 0.002;
+            case SETTLEMENT -> getPrice() * 0.003;
+            case INDUSTRIAL -> getPrice() * 0.005;
+            default -> 0; // если назначение не определено
+        };
+}
+
+}
