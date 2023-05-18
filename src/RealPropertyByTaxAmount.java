50e49ded4154f4169129af13ffa62950f8f5af7e
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-18 14:08:36 +0200

    add class RealPropertyByTaxAmount

---
+++ src/RealPropertyByTaxAmount.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class RealPropertyByTaxAmount implements Comparator<RealProperty> {
+
+    @Override
+    public int compare(RealProperty o1, RealProperty o2) {
+        return Double.compare(o1.getPropertyTax(),o2.getPropertyTax());
+    }
+}
