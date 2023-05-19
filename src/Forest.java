f36b02ebde494887f8ac7bdfdc315ef543410519
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-19 16:44:09 +0200

    add field tree, add method addTree and method removeTree

--- src/Forest.java
+++ src/Forest.java
@@ -1,15 +1,18 @@
 public class Forest extends RealProperty {
     private final boolean isProtectedZone;
+    private int tree;
 
     public Forest(Person owner, String cadastralNumber, double area, double price, String address, boolean isProtectedZone) {
         super(owner, cadastralNumber, area, price, address);
         this.isProtectedZone = isProtectedZone;
+        this.tree = 0;
     }
 
     @Override
     public String toString() {
         return "Forest{" +
                 "isProtectedZone=" + isProtectedZone +
+                ", tree="+tree+
                 "} " + super.toString();
     }
 
@@ -20,5 +23,24 @@ public class Forest extends RealProperty {
         }
         return getPrice() * 0.0025;
     }
+    public void addTree(int tree){
+        this.tree+=tree;
+    }
 
+    public void removeTree(int tree){
+        if (this.tree>=tree){
+            this.tree-=tree;
+        }else{
+         this.tree=0;
+        }
+
+    }
+
+    public int getTree() {
+        return tree;
+    }
+
+    public void setTree(int tree) {
+        this.tree = tree;
+    }
 }
